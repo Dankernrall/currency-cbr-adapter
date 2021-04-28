a8b10dfaa15a3a08a07af5449afb941d1ca55f69
Никита <grand.marchello@gmail.com>
2021-04-28 16:53:30 +0400

    Небольшие правки

--- src/main/java/ru/ds/education/exercise/jms/controller/JmsController.java
+++ src/main/java/ru/ds/education/exercise/jms/controller/JmsController.java
@@ -6,8 +6,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import ru.ds.education.exercise.cbr.model.CurrencyModel;
 import ru.ds.education.exercise.cbr.service.ServiceCbr;
 import ru.ds.education.exercise.jms.mapper.DefaultMapper;
@@ -28,21 +30,24 @@ public class JmsController {
     ServiceCbr serviceCbr;
     @Autowired
     ObjectMapper objectMapper;
-
+    @Autowired
+    JmsTemplate jmsTemplate;
 
     @JmsListener(destination = "RU-DS-EDUCATION-CBR-REQUEST")
     @SendTo("RU-SD-EDUCATION-CBR-RESPONSE")
     public TextMessage getMessage(TextMessage message) throws JMSException, JsonProcessingException, DatatypeConfigurationException {
-        String correlationId = message.getJMSCorrelationID(); //Вытаскиваем CorrelationID
+        String correlationId = null;
+        if (message.getJMSCorrelationID() != null)
+            correlationId = message.getJMSCorrelationID(); //Вытаскиваем CorrelationID
         objectMapper.registerModule(new JavaTimeModule());
         RequestMessage requestMessage = objectMapper.readValue(message.getText(), RequestMessage.class);
         List<CurrencyModel> getCurrency = serviceCbr.cbr(requestMessage.getOnDate().atTime(12, 00)); //Берем дату и отправляем ее с временем 12:00.
         ResponseMessage responseMessage = defaultMapper.map(requestMessage, ResponseMessage.class); //  Так как ЦБ дает вчерашнюю валюту при времени 00:00.
         responseMessage.setCurrencyList(getCurrency); //добавляем все валюты, полученные от ЦБ
         String response = objectMapper.writeValueAsString(responseMessage);
-        if (correlationId != null) //При наличии correlationID, устанавливем его в ответном сообщении
-            message.setJMSCorrelationID(correlationId);
         message.clearBody(); //Очищаем все, чтобы мы смогли записать свою информацию
+        if (correlationId != null)
+            message.setJMSCorrelationID(correlationId);
         message.setText(response); //Запись нашей информации
         return message;
     }
